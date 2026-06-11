0636f3950a93bf95e1e5f58c40ec4e5bb3bc2eaf
Gabriel Gomes <gggabriel368@gmail.com>
2026-06-11 19:35:18 -0300

    server created

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import { router } from './routes/main.js'
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+server.use(router)
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, ()=>{
+    console.log(`Server is running`)
+})
